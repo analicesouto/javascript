856fa7bb4d103c8275a6645ccdb6835b4aef7883
Analice Souto <89554798+analicesouto@users.noreply.github.com>
2025-03-17 22:07:53 -0300

    Create joining_and_splitting_array_items.js

---
+++ exercises/arrays/joining_and_splitting_array_items.js
@@ -0,0 +1,12 @@
+// Crie um array com as palavras ["Olá", "mundo", "JavaScript", "é", "divertido"]; Transforme-a em uma string separada sem espaços; agora, Transforme a string de volta em um array separando por espaços; exiba os resultados no console.
+
+let palavras = ["Olá", "mundo", "JavaScript", "é", "divertido"];
+
+let palavrasSemEspaco = palavras.join(" "); // Sem espaço
+
+let palavrasSeparadas = palavrasSemEspaco.split(" "); // Separando com espaços
+
+console.log(palavrasSemEspaco); 
+
+console.log(palavrasSeparadas); 
+
